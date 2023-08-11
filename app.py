24b0b8dc106d9af92568fcb75eda32db905d48f6
ErikAndSmokey <87280935+ErikAndSmokey@users.noreply.github.com>
2023-08-11 17:00:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
 with open('Mems Network.html', 'r') as f:
 	html_content = f.read()
-	components.html(html_content, height = 800, width = 1200)
+	components.html(html_content, height = 1000, width = 1600)
