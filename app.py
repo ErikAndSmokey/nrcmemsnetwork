513cfa7e35bc8e826acf08d07546dcb42c9e2a02
ErikAndSmokey <87280935+ErikAndSmokey@users.noreply.github.com>
2024-01-05 10:41:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import streamlit.components.v1 as components
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 # st.title('Members Network')
 
-with open('Mems Network 2.html', 'r') as f:
+with open('Mems Network 1.5.24.html', 'r') as f:
 	html_content = f.read()
 	components.html(html_content, height = 800, width = 1200)
